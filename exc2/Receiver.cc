3cf638769601e1f29e13862a434a3cde1d5233fe
Alexander Janßen <alexander.janssen@nithh.de>
2019-04-16 12:57:31 +0200

    Added simple Receiver statistics

--- exc2/Receiver.cc
+++ exc2/Receiver.cc
@@ -4,20 +4,39 @@
 
 using namespace omnetpp;
 
-class Receiver : public cSimpleModule
-{
-  protected:
+class Receiver: public cSimpleModule {
+private:
+    cOutVector receivedTimeVecotr;
+    cDoubleHistogram hopCountStats;
+    double lastMessageReceived;
+protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
+    virtual void finish() override;
 };
 
 Define_Module(Receiver);
 
-void Receiver::initialize()
-{
+void Receiver::initialize() {
+    lastMessageReceived = simTime().dbl();
+    WATCH(lastMessageReceived);
 }
 
-void Receiver::handleMessage(cMessage *msg)
-{
+void Receiver::handleMessage(cMessage *msg) {
     EV << "Received message:" << msg;
+
+    receivedTimeVecotr.record(simTime().dbl() - lastMessageReceived);
+    hopCountStats.collect(simTime().dbl() - lastMessageReceived);
+
+    lastMessageReceived = simTime().dbl();
+}
+
+void Receiver::finish()
+{
+    EV << "Hop count, min:    " << hopCountStats.getMin() << endl;
+    EV << "Hop count, max:    " << hopCountStats.getMax() << endl;
+    EV << "Hop count, mean:   " << hopCountStats.getMean() << endl;
+    EV << "Hop count, stddev: " << hopCountStats.getStddev() << endl;
+
+    hopCountStats.recordAs("hop count");
 }
